e34c536f8fcd0bd758737d396f0918f6117aee0c
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-29 23:37:53 +0300

    refactor: rename to camelCase and use spread operator instead of concat

--- quick_sort/quick_sort.js
+++ quick_sort/quick_sort.js
@@ -1,6 +1,6 @@
 // pivot is the first element of array
 
-function quick_sort(arr) {
+function quickSort(arr) {
   if (arr.length < 2) return arr;
   let pivot = arr[0];
   const right = [];
@@ -10,8 +10,8 @@ function quick_sort(arr) {
     if (num <= pivot) right.push(num)
     else left.push(num);
   });
-  return [].concat(quick_sort(right), [pivot], quick_sort(left));
+  return [...quickSort(right), pivot, ...quickSort(left)];
 }
 
 const arr = [1, 12, 32, 11, 10, 7, 34, 22, 5];
-console.log(quick_sort(arr));
+console.log(quickSort(arr));
