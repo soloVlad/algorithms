744e735bdb100138f1933ce44284640c26c9d350
Vlad Solovey <connectwithvlad@gmail.com>
2024-02-01 21:32:37 +0300

    feat: add set and entries methods for HashMap

--- hash_map/hashMap.js
+++ hash_map/hashMap.js
@@ -1,17 +1,37 @@
 class HashMap {
+  static #MIN_CAPACITY = 16;
+  static #MAX_LOAD = 0.75;
+
+  #load = 0;
+
   constructor() {
-    this.array = new Array(16);
+    this.array = new Array(HashMap.#MIN_CAPACITY);
   }
 
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
 
-    for (let i = 0; i < string.length; i++) {
-      hashCode = primeNumber * hashCode + string.charCodeAt(i);
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
 
     return hashCode;
+  }
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+    const index = keyHash % this.#capacity();
+
+    this.array[index] = [key, value];
+  }
+
+  entries() {
+    return this.array.filter(pair => pair);
+  }
+
+  #capacity() {
+    return this.array.length;
   }
 }
 
--- hash_map/index.js
+++ hash_map/index.js
@@ -1,1 +1,7 @@
 const { HashMap } = require('./hashMap');
+
+const hashMap = new HashMap();
+
+hashMap.set('name', 'vlad');
+
+console.log(hashMap.entries());
