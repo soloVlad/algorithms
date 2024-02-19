3411047026b4a779615135f7d78e96466d058cb2
Vlad Solovey <connectwithvlad@gmail.com>
2024-02-19 16:38:04 +0300

    feat: implement knightMoves function to find path of knight from one cell to another

--- knights_travails/helpers.js
+++ knights_travails/helpers.js
@@ -1,6 +1,8 @@
+const { BOARD_SIZE } = require('./constants');
+
 const isValidCell = (cell) => {
   const isXValid = cell[0] >= 0 && cell[0] < BOARD_SIZE;
-  const isYValid = cell[0] >= 0 && cell[0] < BOARD_SIZE;
+  const isYValid = cell[1] >= 0 && cell[1] < BOARD_SIZE;
 
   return isXValid && isYValid;
 }
--- knights_travails/index.js
+++ knights_travails/index.js
@@ -1,8 +1,12 @@
 const { BOARD_SIZE, POSSIBLE_MOVES } = require('./constants');
 const { isValidCell, isEqualCell } = require('./helpers');
 
-const boardVisited = [...new Array(BOARD_SIZE)]
+let boardVisited = [...new Array(BOARD_SIZE)]
   .map((_) => new Array(BOARD_SIZE).fill(false));
+
+const refreshBoard = () => {
+  boardVisited = boardVisited.map(line => line.fill(false));
+}
 
 const getNextCells = (currentCell) => {
   const validNextCells = [];
@@ -16,12 +20,57 @@ const getNextCells = (currentCell) => {
 
     if (isValidCell(nextCell)) validNextCells.push(nextCell);
   })
+
+  return validNextCells;
+}
+
+const getUnvisitedCells = (arrayOfCells) => {
+  return arrayOfCells.filter(cell => {
+    return !Boolean(boardVisited[cell[0]][cell[1]]);
+  })
+}
+
+const setParentCell = (cell, parentCell) => {
+  boardVisited[cell[0]][cell[1]] = parentCell;
+}
+
+const reconstructPath = (startCell, endCell) => {
+  let currentCell = endCell;
+  const path = [currentCell];
+
+  while (!isEqualCell(currentCell, startCell)) {
+    currentCell = boardVisited[currentCell[0]][currentCell[1]];
+    path.push(currentCell);
+  }
+
+  refreshBoard();
+  return path.reverse();
 }
 
 const knightMoves = (startCell, endCell) => {
-  if (isEqualCell(startCell, endCell)) return [startCell];
+  setParentCell(startCell, startCell);
 
-  const nextCells = getNextCells(startCell);
+  const queue = [startCell];
+
+  while (queue.length) {
+    const currentCell = queue.shift();
+
+    if (isEqualCell(currentCell, endCell)) {
+      break;
+    }
+
+    const nextCells = getNextCells(currentCell);
+    const unvisitedNextCells = getUnvisitedCells(nextCells);
+
+    queue.push(...unvisitedNextCells);
+
+    unvisitedNextCells.forEach(cell => {
+      boardVisited[cell[0]][cell[1]] = currentCell;
+    })
+  }
+
+  return reconstructPath(startCell, endCell);
 }
 
-console.log(boardVisited);
+console.log(knightMoves([0, 0], [3, 3]));
+console.log(knightMoves([3, 3], [0, 0]));
